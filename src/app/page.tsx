d962343037268d4ba4d237669051df30bade376c
Rahul M. Navneeth <50259284+RahulNavneeth@users.noreply.github.com>
2024-07-28 05:20:42 +0530

    [fix] epo (#13)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,18 +5,18 @@ import Main from "@/components/Main";
 import { useState, useEffect } from "react";
 
 export default function Home() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+	const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  useEffect(() => {
-    const username = localStorage.getItem("username");
-    if (!username) {
-      setIsLoggedIn(true);
-    }
-  }, []);
+	useEffect(() => {
+		const username = localStorage.getItem("username");
+		if (username) {
+			setIsLoggedIn(true);
+		}
+	}, []);
 
-  const handleLogin = () => {
-    setIsLoggedIn(true);
-  };
+	const handleLogin = () => {
+		setIsLoggedIn(true);
+	};
 
-  return <>{isLoggedIn ? <Main /> : <Login onLogin={handleLogin} />}</>;
+	return <>{isLoggedIn ? <Main /> : <Login onLogin={handleLogin} />}</>;
 }
